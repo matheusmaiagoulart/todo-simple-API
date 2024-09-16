17b0fd037cbcd26770dfc7e928ba969e32c2dc9c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2024-09-16 13:27:05 -0300

    Spring Boot Project

---
+++ todosimple/src/test/java/com/matheusmaia/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmaia.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
